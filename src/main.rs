d56ef1a4edd8537957095f56b471b93b2e4b7f2a
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-27 01:18:40 +0200

    ✨ feat(Cargo.toml): Update dependencies and package name 🚀 feat(src/main.rs): Implement WebSocket server with graceful shutdown
    📝 Summary:
    In the Cargo.toml file, updated dependencies and renamed the package to "websocket_server" for clarity.
    
    In the src/main.rs file, implemented a WebSocket server that listens on 127.0.0.1:8080 and handles incoming WebSocket connections. It also provides graceful shutdown functionality in response to SIGINT.
    
    🔍 Details:
    **Cargo.toml:**
    - Updated dependencies, including futures, serde, serde_json, and tokio, to the specified versions.
    
    **src/main.rs:**
    - Created a function `handle_client` to handle WebSocket connections.
    - Implemented WebSocket server logic using tokio and tokio_tungstenite.
    - Added a graceful shutdown mechanism that responds to SIGINT (Ctrl+C).
    
    ➕ Dependencies:
    - Updated the versions of the following dependencies:
      - futures: 0.3.29
      - serde: 1.0.190 (with the "derive" feature)
      - serde_json: 1.0.107
      - tokio: 1.33.0 (with the "full" feature)
    
    🐛 Bug Fix:
    - N/A
    
    ✨ Feature Enhancement:
    - Added WebSocket server functionality.
    - Implemented graceful shutdown in response to SIGINT.
    
    📚 Challenges:
    - N/A
    
    ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+
+use futures::{SinkExt, StreamExt};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    signal, task,
+};
+use tokio_tungstenite::tungstenite::Message;
+
+async fn handle_client(stream: TcpStream) {
+    let mut ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during handshake");
+
+    loop {
+        let msg = ws_stream
+            .next()
+            .await
+            .expect("Error reading message")
+            .expect("No message received");
+
+        match msg {
+            Message::Text(text) => {
+                println!("Received text message: {}", text);
+                ws_stream
+                    .send(Message::Text(text))
+                    .await
+                    .expect("Error sending message");
+            }
+            Message::Close(_) => {
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080")
+        .await
+        .expect("Failed to bind");
+
+    println!("Server is listening on 127.0.0.1:8080");
+
+    task::spawn(async move {
+        signal::ctrl_c().await.expect("Failed to bind SIGINT");
+        println!("Received SIGINT, shutting down gracefully...");
+        process::exit(0);
+    });
+
+    while let Ok((stream, _)) = listener.accept().await {
+        task::spawn(handle_client(stream));
+    }
 }
