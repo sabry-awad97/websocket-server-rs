204ca8569f182b0618a3f6f17a2e90bd4a104c95
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-27 02:20:08 +0200

    📦 feat(main.rs): Implement WebSocket server with event handling 📝 Summary: Added WebSocket server functionality with event handling, enabling the server to process different types of WebSocket events (e.g., "join" and "message"). The server can now respond to incoming messages by invoking the appropriate event callback. 💡 Details: - Defined an enum `EventType` to categorize WebSocket events as "Join" or "Message." - Created a struct `Event<T>` to represent WebSocket events, where `T` is the payload type. - Implemented a `WebSocketServer<P>` struct, parametrized by the payload type `P`. - The `handle_client` method now processes WebSocket messages, identifies their event type, and triggers the associated event callback. - Introduced an `on` method to register event callbacks for specific event types. - The `start` method initiates the WebSocket server, and the server is configured to respond to "message" events with a simple print statement. 🔍 Context: - The WebSocket server was developed to support WebSocket communication, categorize incoming events, and execute the appropriate event callback. - This enhancement is valuable for building applications that require real-time communication. ✨ Feature Enhancement: - The WebSocket server can now handle different event types, making it versatile for various use cases. 📚 Challenges: - N/A ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
-use std::process;
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::{process, sync::Mutex};
 
 use futures::StreamExt;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
 use tokio::{
     net::{TcpListener, TcpStream},
     signal, task,
 };
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
-// Define a struct to represent the WebSocket server
-#[derive(Debug, Clone)]
-struct WebSocketServer {
-    listener: Arc<TcpListener>,
+#[derive(Debug, Deserialize, Serialize, Clone, Eq, Hash, PartialEq)]
+enum EventType {
+    Join,
+    Message,
 }
 
-impl WebSocketServer {
+impl From<&str> for EventType {
+    fn from(s: &str) -> Self {
+        match s {
+            "join" => EventType::Join,
+            "message" => EventType::Message,
+            _ => panic!("Unknown event type: {}", s),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Event<T> {
+    #[serde(rename = "type")]
+    r#type: EventType,
+    payload: T,
+}
+
+#[derive(Clone)]
+struct WebSocketServer<P> {
+    listener: Arc<TcpListener>,
+
+    #[allow(clippy::type_complexity)]
+    event_callbacks: Arc<Mutex<HashMap<EventType, Box<dyn Fn(Event<P>) + Send>>>>,
+}
+
+impl<P> WebSocketServer<P>
+where
+    P: DeserializeOwned + Clone + 'static,
+{
     async fn new(address: &str) -> Self {
         let listener = TcpListener::bind(address).await.expect("Failed to bind");
-
         Self {
             listener: listener.into(),
+            event_callbacks: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    // Method to handle a client's connection
     async fn handle_client(&self, stream: TcpStream) {
-        let mut ws_stream = accept_async(stream).await.expect("Error during handshake");
+        let ws_stream = accept_async(stream).await.expect("Error during handshake");
+        let (_, mut receiver) = ws_stream.split();
 
+        let callbacks = Arc::clone(&self.event_callbacks);
         loop {
-            let msg = ws_stream
+            let msg = receiver
                 .next()
                 .await
                 .expect("Error reading message")
@@ -36,7 +68,12 @@ impl WebSocketServer {
 
             match msg {
                 Message::Text(text) => {
-                    println!("Received text message: {}", text.trim());
+                    if let Ok(event) = serde_json::from_str::<Event<P>>(&text) {
+                        if let Some(callback) = callbacks.lock().unwrap().get(&event.r#type.clone())
+                        {
+                            callback(event);
+                        }
+                    }
                 }
                 Message::Binary(bin) => {
                     println!("Received Binary message: {:?}", bin);
@@ -49,7 +86,14 @@ impl WebSocketServer {
         }
     }
 
-    // Method to start the server and handle incoming connections
+    fn on<F>(&self, event_type: EventType, callback: F)
+    where
+        F: Fn(Event<P>) + Send + 'static,
+    {
+        let mut callbacks = self.event_callbacks.lock().unwrap();
+        callbacks.insert(event_type, Box::new(callback));
+    }
+
     async fn start(&self) {
         println!(
             "Server is listening on {}",
@@ -75,6 +119,10 @@ impl WebSocketServer {
 #[tokio::main]
 async fn main() {
     let server = WebSocketServer::new("127.0.0.1:8080").await;
+
+    server.on(EventType::Message, |event: Event<String>| {
+        println!("Received text message: {}", event.payload.trim());
+    });
 
     task::spawn(async move {
         signal::ctrl_c().await.expect("Failed to bind SIGINT");
